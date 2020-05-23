d717d557e2aac2a7a0e971dd51f7340ab155e23d
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-05-23 16:01:40 +0530

    Scopes of the var and let
    Compare Scopes of the var and let Keywords

---
+++ letScope.js
@@ -0,0 +1,22 @@
+function checkScope() {
+  'use strict';
+  let i = 'function scope';
+  if (true) {
+    let i = 'block scope';
+    console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i;
+}
+
+
+var printNumTwo;
+for (var i = 0; i < 3; i++) {
+  if (i === 2) {
+    printNumTwo = function() {
+      return i;
+    };
+  }
+}
+console.log(printNumTwo());
+// returns 3
