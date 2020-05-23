6ef0d0a0e8a90972e73082b2d014610d0a1d9587
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-05-23 15:50:15 +0530

    let Keywords
    Explore Differences Between the var and let Keywords

---
+++ let.js
@@ -0,0 +1,18 @@
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
+
+var camper = 'James';
+var camper = 'David';
+console.log(camper);
+// logs 'David'
+
+let camper = 'James';
+let camper = 'David'; // throws an error
