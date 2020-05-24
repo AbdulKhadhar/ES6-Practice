8e46d159c1db5b1f8da1c2916aedaace82e8c7b4
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-05-24 12:54:05 +0530

    Create Strings using Template Literals

---
+++ TemplateLiteralsStr.js
@@ -0,0 +1,31 @@
+const person = {
+  name: "Zodiac Hasbro",
+  age: 56
+};
+// Template literal with multi-line and string interpolation
+const greeting = `Hello, my name is ${person.name}!
+I am ${person.age} years old.`;
+
+console.log(greeting); // prints
+// Hello, my name is Zodiac Hasbro!
+// I am 56 years old.
+
+
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  const resultDisplayArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    resultDisplayArray.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+
+  return resultDisplayArray;
+}
+
+const resultDisplayArray = makeList(result.failure);
