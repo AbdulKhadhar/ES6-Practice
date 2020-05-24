e17c06f8f5d953b5bf02aacf7e488b4dd0311e5f
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-05-24 13:39:26 +0530

    Write Concise Declarative Functions with ES6

---
+++ ConciseDeFun.js
@@ -0,0 +1,23 @@
+const person = {
+  name: "Taylor",
+  sayHello: function() {
+    return `Hello! My name is ${this.name}.`;
+  }
+};
+const person = {
+  name: "Taylor",
+  sayHello() {
+    return `Hello! My name is ${this.name}.`;
+  }
+};
+
+
+
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+bicycle.setGear(3);
+console.log(bicycle.gear);
