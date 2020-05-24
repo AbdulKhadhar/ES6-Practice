a0e9ed3e31e2107fc5785a955df142caf28dfd9e
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-05-24 21:11:38 +0530

    Create an Export Fallback with export default

---
+++ exportDefault.js
@@ -0,0 +1,15 @@
+// named function
+export default function add(x, y) {
+  return x + y;
+}
+
+// anonymous function
+export default function(x, y) {
+  return x + y;
+}
+
+
+
+export default function(x, y) {
+  return x - y;
+}
