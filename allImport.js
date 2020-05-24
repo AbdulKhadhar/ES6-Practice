1e2e8427b81bfce61c417ff09f7b7bfc55165e66
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-05-24 21:05:53 +0530

    Use * to Import Everything from a File

---
+++ allImport.js
@@ -0,0 +1,8 @@
+import * as myMathModule from "./math_functions.js";
+import * as stringFunctions from "./string_functions.js"
+
+
+myMathModule.add(2,3);
+myMathModule.subtract(5,3);
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
