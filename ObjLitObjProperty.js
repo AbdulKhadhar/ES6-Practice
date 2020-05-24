d87a0f49570ea6d749416f803c5b5371d32ae79b
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-05-24 13:01:09 +0530

    Object Literal Declarations Using Object Property
    Write Concise Object Literal Declarations Using Object Property Shorthand

---
+++ ObjLitObjProperty.js
@@ -0,0 +1,12 @@
+const getMousePosition = (x, y) => ({
+  x: x,
+  y: y
+});
+const getMousePosition = (x, y) => ({ x, y });
+
+
+
+const createPerson = (name, age, gender) => {
+  "use strict";
+  return {name,age,gender};
+};
