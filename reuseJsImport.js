b11fc7b1f735382430104000f74784f87fb44d3c
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-05-24 21:02:24 +0530

    Reuse JavaScript Code Using import

---
+++ reuseJsImport.js
@@ -0,0 +1,7 @@
+import { add, subtract } from './math_functions.js';
+add(1,2);
+
+
+import {uppercaseString , lowercaseString }  from './string_functions.js'
+uppercaseString("hello");
+lowercaseString("WORLD!");
